35ec52a3ff585c464eb904055ace6d56668d3f55
Liam Hall <40840255+wearethreebears@users.noreply.github.com>
2022-06-01 21:43:17 +0100

    Remove incorrect local

--- local/Column/Column.classes.ts
+++
@@ -1,9 +0,0 @@
-import type { PropType } from "vue";
-import type { ClassProps } from "@barebones/types/prop";
-
-export const classProps: ClassProps = {
-    span: {
-        type: [String, Array] as PropType<string | string[]>,
-        default: "default",
-    },
-};
--- local/Image/Image.classes.ts
+++
@@ -1,9 +0,0 @@
-import type { PropType } from "vue";
-import type { ClassProps } from "@barebones/types/prop";
-
-export const classProps: ClassProps = {
-    width: {
-        type: String as PropType<string>,
-        default: "default",
-    },
-};
